f59ec4a646f89725e4f07acc91b762f208a99255
yadavankit <connectankityadav@gmail.com>
2016-02-10 03:12:41 +0530

    Adds unicode for Models

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 class Question(models.Model):
+	#Unicode for Question
+	def __str__(self):
+		return self.question_text
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
+	#Unicode for Choice
+	def __str__(self):
+		return self.choice_text
 	question = models.ForeignKey(Question)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
