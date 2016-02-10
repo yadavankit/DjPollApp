25cd2de0be42a928c91cd93d7ce92a8e99f756d4
yadavankit <connectankityadav@gmail.com>
2016-02-11 01:52:50 +0530

    Register models

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+class ChoiceInline(admin.StackedInline):
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [(None, {'fields':['question_text']}),('Date Info', {'fields': ['pub_date']})]
+	inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
